3e534387df1ebef983d74660bf27381c62b7c3e6
thomas mouchelet <mouchelet.thomas@gmail.com>
2020-03-18 16:37:39 +0100

    add socket io

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var port = process.env.PORT || 3000;
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function () {
-    console.log('listening on *:' + port);
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
